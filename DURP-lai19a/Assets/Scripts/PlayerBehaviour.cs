59e3b0a4afb0577ecf56a3ada6104f8abfd7cac0
Luibarra <lai19a@acu.edu>
2022-02-16 21:56:37 -0600

    Main Commit
    Menu Creation Chapter

---
+++ DURP-lai19a/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        // If the game is paused, don't do anything
+        if (PauseScreenBehaviour.paused)
+        {
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // If the game is paused, don't do anything
+        if (PauseScreenBehaviour.paused)
+        {
+            return;
+        }
+
+    }
+}
